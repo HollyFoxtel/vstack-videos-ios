d47bc7f2b530ec231ac6f8bf874352ebb19b4a74
Holly <hao.li@foxtel.com.au>
2024-08-19 16:02:36 +0800

    Make sure it always focus the first time if there are more content

--- VideoStackPOC/VideoStackPOC/Home/TileContainer.swift
+++ VideoStackPOC/VideoStackPOC/Home/TileContainer.swift
@@ -3,20 +3,31 @@ import SwiftUI
 struct TileContainer<Asset: AssetProtocol>: View {
     let assets: [Asset]
     
+    @FocusState private var focusedIndex: Asset.ID?
+    
     var body: some View {
-        ScrollView(.horizontal) {
-            LazyHStack(spacing: 40) {
-                ForEach(assets) { asset in
-                    Button {} label: {
-                        asset.image
-                            .resizable()
-                            .aspectRatio(768 / 432, contentMode: .fit)
-                            .containerRelativeFrame(.horizontal, count: 5, spacing: 50)
+        ScrollViewReader { scrollProxy in
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 40) {
+                    ForEach(assets) { asset in
+                        Button {} label: {
+                            asset.image
+                                .resizable()
+                                .aspectRatio(768 / 432, contentMode: .fit)
+                                .containerRelativeFrame(.horizontal, count: 5, spacing: 50)
+                        }
+                        .focused($focusedIndex, equals: asset.id)
+                        .id(asset.id)
                     }
                 }
             }
+            .scrollClipDisabled()
+            .buttonStyle(.borderless)
+            .onChange(of: focusedIndex) { newID in
+                withAnimation {
+                    scrollProxy.scrollTo(newID, anchor: .leading)
+                }
+            }
         }
-        .scrollClipDisabled()
-        .buttonStyle(.borderless)
     }
 }
